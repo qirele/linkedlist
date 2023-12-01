111294fbb0340a98e88b9bb912f4bc8de7a16492
Kiril Hizhavsky <79530158+qirele@users.noreply.github.com>
2023-12-01 12:09:14 +0100

    add getHead, append methods

--- main.js
+++ main.js
@@ -1,9 +1,38 @@
 function LinkedList() {
-  const list = null;
+  let head = null;
+
+  const getHead = () => head;
+
+  const append = (value) => {
+    if (head === null) {
+      head = Node(value);
+      return;
+    }
+
+    let cur = head;
+    let prev = null;
+    while (cur !== null) {
+      prev = cur;
+      cur = cur.next;
+    }
+
+    prev.next = Node(value);
+  }
+
+
+  return {getHead, append};
 }
 
-function Node() {
-  const value = null;
-  const next = null;
+function Node(value = null, next = null) {
+  // let obj = {};
+  // obj.value = value;
+  // obj.next = next;
+  // return obj;
   return {value, next}
 }
+
+let list = LinkedList();
+list.append("bruh");
+list.append("babe");
+list.append("dawg");
+console.log(list.getHead());
